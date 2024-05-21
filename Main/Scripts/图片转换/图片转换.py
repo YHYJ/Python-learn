4058f1d5a730a18793a6762456b6af38689552ce
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-05-21 09:18:02 +0800

    替换为环境变量值

--- Main/Scripts/图片转换/图片转换.py
+++ Main/Scripts/图片转换/图片转换.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """转换图片的大小和类型"""
 
+import os
 from PIL import Image
 
 picture_path = input('输入图片绝对路径(类似/home/picture.png):')
@@ -15,8 +15,10 @@ picture_lsize = int(input('请输入目标图片的长度:'))
 picture_wsize = int(input('请输入目标图片的宽度:'))
 picture_mode = input('请输入目标图片的类型(jpg,png,bmp,):')
 im.thumbnail((picture_lsize, picture_wsize))
+
+home = os.environ.get('HOME')
 if picture_mode == 'jpg':
-    im.save('/home/yj/Pictures/Conversion_results.jpg', 'jpeg')
+    im.save('{}/Pictures/Conversion_results.jpg'.format(home), 'jpeg')
 else:
-    im.save('/home/yj/Pictures/Conversion_results.%s' % picture_mode,
-            '%s' % picture_mode)
+    im.save('{}/Pictures/Conversion_results.{}'.format(home, picture_mode),
+            picture_mode)
